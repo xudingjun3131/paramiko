d090a784276ae7b817d9b0a1f3fc5f6c699b7d10
xudingjun3131 <xudingjun3131@16.com>
2017-07-27 14:04:53 +0800

    123
    Signed-off-by: xudingjun3131 <xudingjun3131@16.com>

--- fabric/simple3.py
+++ fabric/simple3.py
@@ -2,15 +2,34 @@ from fabric.api import *
 from fabric.context_managers import *
 from fabric.contrib.console import confirm
 
-env.user = 'root'
+env.user = 'xudj'
 env.gateway = '192.168.213.161'
 env.hosts = '192.168.213.161'
+env.password = '123456'
 
 env.passwords = {
-    'root@192.168.213.161:22': '123456',
-    'xudj@192.168.213.161:22': '123456',
-    'amber@192.168.213.161:22': '123456'
+    'zabbix@192.168.213.161:22': 'zabbix',
 }
 
-lpackpath = 'E:\Python\paramiko_work\sftp'
-rpackpath = '/tmp/'
+lpackpath = "/home/xudj/test/sftp.tar.gz"
+rpackpath = "/home/zabbix"
+
+@task
+def put_task():
+    run("mkdir -p /home/zabbix")
+    with settings(warn_only=True):
+        result = put(lpackpath,rpackpath)
+    if result.failed and not confirm("put file failed,Continue[Y/N]?"):
+        abort("Aborting file put task!")
+
+@task
+def run_task():
+    with cd("/home/zabbix"):
+        run("tar -zxvf sftp.tar.gz")
+        with cd("zabbix/"):
+            run("./start.sh")
+
+@task
+def go():
+    put_task()
+    run_task()
